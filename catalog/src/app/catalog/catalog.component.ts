fa32570a91034aebe3d53474d58657b45cf08209
Hakan Dilek <hakandilek@gmail.com>
2023-02-28 14:42:13 +0100

    catalog: log component init calls

--- catalog/src/app/catalog/catalog.component.ts
+++ catalog/src/app/catalog/catalog.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
   count: number;
 
   constructor() {
+    console.log(`catalog: CatalogComponent()`);
     this.count = 0;
+  }
+
+  ngOnInit(): void {
+    console.log(`catalog: CatalogComponent.ngOnInit()`);
   }
 
   increment() {
