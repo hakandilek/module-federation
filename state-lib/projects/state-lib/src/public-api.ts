0e017db0bdc0381c3a712812928d3f9416088ac6
Hakan Dilek <hakandilek@gmail.com>
2023-03-08 22:20:44 +0100

    state: state-lib is pure js lib remove angular dependency

--- state-lib/projects/state-lib/src/public-api.ts
+++ state-lib/projects/state-lib/src/public-api.ts
@@ -2,7 +2,5 @@
  * Public API Surface of state-lib
  */
 
-export * from './lib/state.service';
-export * from './lib/state.module';
 export * from './lib/store';
 export * from './lib/store-utils';
